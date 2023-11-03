09f859c0b9e1ef32b65f7e0c1dc5749fc763dfc3
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-11-03 15:40:22 +0000

    100-textme.rb

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+## This script accepts one argument and passes it to a regular expression
+
+sender = ARGV[0].scan(/(?<=\[from:)[^ ]+(?=\])/).join
+reciever = ARGV[0].scan(/(?<=\[to:)[^ ]+(?=\])/).join
+flags = ARGV[0].scan(/(?<=\[flags:)[^ ]+(?=\])/).join
+puts "#{sender},#{reciever},#{flags}"
