2f72598c379edc2cca17e4e6346243b09abc6408
udofiaisrael1990 <udofiaisrael170@gmail.com>
2024-02-17 09:46:45 +0000

    corrections

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-file { '/var/www/html/wp-settings.php':
-ensure  => file,
-content => file('/var/www/html/wp-settings.php').content.gsub('phpp', 'php'),
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
